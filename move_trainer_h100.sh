e8a15a3e7026388b6187cabdd94e7871ab0b80e0
Hank Kung <hank910140@gmail.com>
2025-01-29 13:18:34 -0500

    slurm scripts

---
+++ move_trainer_h100.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+if [ "$1" = "" ]; then
+    echo "Job name cannot be empty"
+    exit 1
+fi
+
+export JOB_NAME=$1_$(date '+%Y-%m-%d_%H:%M:%S')
+export DESTINATION_DIR='experiments'
+
+mkdir $DESTINATION_DIR/$JOB_NAME/
+cp -R base/ $DESTINATION_DIR/$JOB_NAME/
+cp -R configs/ $DESTINATION_DIR/$JOB_NAME/
+cp -R data_loader/ $DESTINATION_DIR/$JOB_NAME/
+cp -R logger/ $DESTINATION_DIR/$JOB_NAME/
+cp -R model/ $DESTINATION_DIR/$JOB_NAME/
+cp -R run/ $DESTINATION_DIR/$JOB_NAME/
+cp -R trainer/ $DESTINATION_DIR/$JOB_NAME/
+cp -R utils/ $DESTINATION_DIR/$JOB_NAME/
+cp parse_config.py $DESTINATION_DIR/$JOB_NAME/
+cp distributed_main.py $DESTINATION_DIR/$JOB_NAME/
+cp trainer_clip_h100.sh $DESTINATION_DIR/$JOB_NAME/trainer_local.sh
+
+cd $DESTINATION_DIR/$JOB_NAME/
+
+sbatch -A r01220 -p hopper -J $JOB_NAME trainer_local.sh
---
+++ trainer_clip_v100.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+#SBATCH --partition=gpu
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=256G
+#SBATCH --cpus-per-task=32
+#SBATCH --time=48:00:00
+#SBATCH --output=slurm/%x-%j.out
+#SBATCH --error=slurm/%x-%j.err
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+MASTER_PORT=6000
+GPUS_PER_NODE=2
+NNODES=$SLURM_NNODES
+
+export JOB_NAME=$SLURM_JOB_NAME
+export NCCL_DEBUG=INFO
+
+#module load conda
+module --ignore-cache load "conda"
+source ~/.bashrc
+conda activate hiervl
+module load ffmpeg
+# Print allocated GPUs for debugging
+echo "Allocated GPUs:"
+
+srun python distributed_main.py --multiprocessing-distributed --config ./configs/pt/clip_cf_v100.json --experiment egoclip_cf
