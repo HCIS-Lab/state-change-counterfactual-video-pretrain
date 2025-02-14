a19d30241d506a3e717e1d21d8e909b0e21ae5b3
Hank Kung <hank910140@gmail.com>
2025-02-13 19:41:15 -0500

    Update vit.py

--- model/vit.py
+++ model/vit.py
@@ -248,7 +248,7 @@ def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
 class VisionTransformer(nn.Module):
     """ Vision Transformere
     """
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=256, depth=6,
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=6,
                  num_heads=8, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
                  drop_path_rate=0.1, hybrid_backbone=None, norm_layer=nn.LayerNorm, num_frames=8, attention_type='divided_space_time', dropout=0.):
         super().__init__()
