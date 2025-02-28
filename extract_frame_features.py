23d85bfdaf8fbbcb69fdc561f70bf420984c136b
Hank Kung <hank910140@gmail.com>
2025-02-28 17:55:30 -0500

    Update extract_frame_features.py

--- extract_frame_features.py
+++ extract_frame_features.py
@@ -141,6 +141,8 @@ def main():
                     
                     if non_splt :
                         window_size = 15
+                        if args.model == 'milnce':
+                            window = 16
                         # [b, 96, 224, 224]
                         window = window.view((-1, config.data.num_frames, 3) + window.size()[-2:])
                         # [b, 32, 3, 224, 224]
@@ -149,8 +151,17 @@ def main():
                         # [1, 15, 3, 224, 224]
                         first_frame = window[:, 0:1, :, :, :]  # Shape: [b, 1, C, H, W]
                         last_frame = window[:, -1:, :, :, :]   # Shape: [b, 1, C, H, W]
-                        front_padding = first_frame.repeat(1, (window_size-1)//2, 1, 1, 1)  # Shape: [b, 7, C, H, W]
-                        rear_padding = last_frame.repeat(1, (window_size-1)//2, 1, 1, 1)    # Shape: [b, 7, C, H, W]
+
+                        if window_size % 2 == 1:
+                            front_padding = first_frame.repeat(1, (window_size-1)//2, 1, 1, 1)
+
+                            rear_padding = last_frame.repeat(1, (window_size-1)//2, 1, 1, 1)
+                        else:
+                            front_pad = window_size // 2 - 1
+                            rear_pad  = window_size // 2
+                            front_padding = first_frame.repeat(1, front_pad, 1, 1, 1)
+                            rear_padding  = last_frame.repeat(1, rear_pad, 1, 1, 1)
+
                         padded_video = torch.cat([front_padding, window, rear_padding], dim=1)
                         # [b, T+14, c, h , w]
                         b, T, C, H, W = padded_video.shape  # T = 24 (10 original + 7 front + 7 rear)
@@ -160,8 +171,11 @@ def main():
                         window = window.to(device)
                         if args.model == 'hiervl' or 'cf' in args.model:
                             feature = model(data=window, video_only=True)
-                        else:
+                        elif args.model == 'milnce':
+                            window = window.permute(0,2,1,3,4)
                             feature = model(window)
+                            feature = feature['mixed_5c']
+
                         # [b*num_frames, c]
                         feature = feature.reshape(b, config.data.num_frames, -1)
                         feature = feature.permute(0,2,1)
