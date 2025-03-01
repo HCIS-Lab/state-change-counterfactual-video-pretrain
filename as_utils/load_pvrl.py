edc2494b1835b67cc6fc9538ae351ce5a93d0d7f
frangilr <154013745+frangilr@users.noreply.github.com>
2025-02-28 22:02:25 -0500

    pvrl model load

---
+++ as_utils/load_pvrl.py
@@ -0,0 +1,147 @@
+import torch
+import os
+from collections import OrderedDict
+# from fvcore.common.registry import Registry
+import argparse
+from pvrl_defaults import get_cfg
+# import sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from lib.models import vit 
+
+# adapted from https://github.com/facebookresearch/ProcedureVRL/tree/main#zero-shot-inference
+def load_state_dict(checkpoint_path, use_ema=False):
+    if checkpoint_path and os.path.isfile(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path, map_location='cpu')
+        state_dict_key = 'state_dict'
+        if isinstance(checkpoint, dict):
+            if use_ema and 'state_dict_ema' in checkpoint:
+                state_dict_key = 'state_dict_ema'
+        if state_dict_key and state_dict_key in checkpoint:
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint[state_dict_key].items():
+                # strip `module.` prefix
+                name = k[7:] if k.startswith('module') else k
+                new_state_dict[name] = v
+            state_dict = new_state_dict
+        elif 'model_state' in checkpoint:
+            state_dict_key = 'model_state'
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint[state_dict_key].items():
+                # strip `model.` prefix
+                # name = k[6:] if k.startswith('model') else k
+                name = k
+                new_state_dict[name] = v
+            state_dict = new_state_dict
+        else:
+            state_dict = checkpoint
+        return state_dict
+    else:
+        raise FileNotFoundError()
+
+def parse_args():
+    """
+    Parse the following arguments for a default parser for PySlowFast users.
+    Args:
+        shard_id (int): shard id for the current machine. Starts from 0 to
+            num_shards - 1. If single machine is used, then set shard id to 0.
+        num_shards (int): number of shards using by the job.
+        init_method (str): initialization method to launch the job with multiple
+            devices. Options includes TCP or shared file-system for
+            initialization. details can be find in
+            https://pytorch.org/docs/stable/distributed.html#tcp-initialization
+        cfg (str): path to the config file.
+        opts (argument): provide addtional options from the command line, it
+            overwrites the config loaded from file.
+    """
+    parser = argparse.ArgumentParser(
+        description="Provide SlowFast video training and testing pipeline."
+    )
+    parser.add_argument(
+        "--shard_id",
+        help="The shard id of current node, Starts from 0 to num_shards - 1",
+        default=0,
+        type=int,
+    )
+    parser.add_argument(
+        "--num_shards",
+        help="Number of shards using by the job",
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        "--init_method",
+        help="Initialization method, includes TCP or shared file-system",
+        default="tcp://localhost:9999",
+        type=str,
+    )
+    parser.add_argument(
+        "--cfg",
+        dest="cfg_file",
+        help="Path to the config file",
+        default="pvrl.yaml",
+        type=str,
+    )
+    parser.add_argument(
+        "opts",
+        help="See slowfast/config/defaults.py for all options",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+
+    return parser.parse_args()
+
+
+def load_config(args):
+    """
+    Given the arguemnts, load and initialize the configs.
+    Args:
+        args (argument): arguments includes `shard_id`, `num_shards`,
+            `init_method`, `cfg_file`, and `opts`.
+    """
+    # Setup cfg.
+    cfg = get_cfg()
+    # Load config from cfg.
+    if args.cfg_file is not None:
+        cfg.merge_from_file(args.cfg_file)
+    # Load config from command line, overwrite config from opts.
+    if args.opts is not None:
+        cfg.merge_from_list(args.opts)
+
+    # Inherit parameters from args.
+    if hasattr(args, "num_shards") and hasattr(args, "shard_id"):
+        cfg.NUM_SHARDS = args.num_shards
+        cfg.SHARD_ID = args.shard_id
+    if hasattr(args, "rng_seed"):
+        cfg.RNG_SEED = args.rng_seed
+    if hasattr(args, "output_dir"):
+        cfg.OUTPUT_DIR = args.output_dir
+
+    # Create the checkpoint dir.
+    return cfg
+
+def load_pvrl(model_path):
+    """
+    Load from saved checkpoints
+
+    :param model_path: Checkpoint path to be loaded
+    """
+    args = parse_args()
+    cfg = load_config(args)
+
+    # Construct the model
+    model = vit.vit_base_patch16_224_develop(cfg)
+
+    state_dict = load_state_dict(model_path)
+    model.load_state_dict(state_dict, strict=False)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    print("Checkpoint loaded.")
+
+    return model
+
+if __name__ == "__main__":
+    # model = load_pvrl("/nfs/wattrel/data/md0/datasets/state_aware/pvrl/model_epoch_00025.pyth")
+    # print(model)
+    raise Exception("I should not be main")
